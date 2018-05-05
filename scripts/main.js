3146de5614e2d9d8ebf08e048dcdb6d72888e3d9
Dominic Looser <dominic.looser@gmail.com>
2018-05-05 02:40:29 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 (function() {
   $('.hamburger').click(function() {
-    return $('#mobile-nav').show();
+    return $('.mobile-nav').show();
+  });
+
+  $('.close').click(function() {
+    return $('.mobile-nav').hide();
   });
 
 }).call(this);
